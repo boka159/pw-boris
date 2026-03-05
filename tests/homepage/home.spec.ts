273324efbc6027ddaa43fb25ed497713000b6bca
BorisPrototyp <boris@prototyp.digital>
2026-03-05 21:29:23 +0100

    finished home screen basic tests

--- tests/homepage/home.spec.ts
+++ tests/homepage/home.spec.ts
@@ -38,3 +38,47 @@ test("has card title and price", async ({ page }) => {
   );
   await expect(page.locator(".card-text")).toHaveCount(cardCount);
 });
+
+test("has working filters", async ({ page }) => {
+  await expect(page.locator(".list-group-item")).toContainText([
+    "CATEGORIES",
+    "Phones",
+    "Laptops",
+    "Monitors",
+  ]);
+
+  await page.locator(".card").first().waitFor();
+  const totalCount = await page.locator(".card-block").count();
+  console.log(totalCount);
+
+  await page.locator(".list-group-item", { hasText: "Phones" }).click();
+  await expect(page.locator(".card")).not.toHaveCount(totalCount);
+
+  const filterCount = await page.locator(".card-block").count();
+  console.log(filterCount);
+
+  expect(filterCount).toBeLessThan(totalCount);
+});
+
+test("product card leads to product page", async ({ page }) => {
+  const productTitle = await page.locator(".card-title").first().innerText();
+  await page.locator(".card-img-top").first().click();
+
+  await expect(page.locator(".name")).toHaveText(productTitle);
+});
+
+test.only("has working pagination", async ({ page }) => {
+  await page.locator(".card").first().waitFor();
+  const pageOneProduct = await page.locator(".card-title").last().innerText();
+  console.log(pageOneProduct);
+
+  await page.locator(".page-item #next2", { hasText: "Next" }).click();
+  await expect(page.locator(".card-title").last()).not.toHaveText(
+    pageOneProduct,
+  );
+
+  await page.locator(".card").first().waitFor();
+  await page.locator(".page-item #prev2", { hasText: "Previous" }).click();
+
+  await expect(page.locator(".card-title")).toContainText([pageOneProduct]);
+});
