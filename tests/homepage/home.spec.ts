86031c2eca06ffcd6b5718c82e9a473df3b7cfb1
BorisPrototyp <boris@prototyp.digital>
2026-03-07 19:35:16 +0100

    added tests for filters, added test data, removed waitFor(), cleaned config file

--- tests/homepage/home.spec.ts
+++ tests/homepage/home.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
+import { CATEGORIES } from "../../testData";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("https://www.demoblaze.com/");
+  await page.goto("/");
 });
 
 test("has title", async ({ page }) => {
@@ -9,7 +10,7 @@ test("has title", async ({ page }) => {
 });
 
 test("has nav logo and links", async ({ page }) => {
-  await expect(page.locator(".navbar-brand")).toHaveText("PRODUCT STORE");
+  await expect.soft(page.locator(".navbar-brand")).toHaveText("PRODUCT STORE");
   await expect(page.locator("#navbarExample a")).toContainText([
     "Home",
     "Contact",
@@ -21,40 +22,48 @@ test("has nav logo and links", async ({ page }) => {
 });
 
 test("has products", async ({ page }) => {
-  await page.locator(".card").first().waitFor();
+  await expect.soft(page.locator(".card").first()).toBeVisible();
   const count = await page.locator(".card").count();
 
-  await expect(count).toBeGreaterThan(0);
+  await expect(count).toEqual(9);
 });
 
 test("has card title and price", async ({ page }) => {
-  await page.locator(".card").first().waitFor();
+  await expect.soft(page.locator(".card").first()).toBeVisible();
 
   const cardCount = await page.locator(".card-block").count();
-  await expect(page.locator(".card-title")).toHaveCount(cardCount);
-  await expect(page.locator(".card-block h5", { hasText: "$" })).toHaveCount(
-    cardCount,
-  );
+  await expect.soft(page.locator(".card-title")).toHaveCount(cardCount);
+  await expect
+    .soft(page.locator(".card-block h5", { hasText: "$" }))
+    .toHaveCount(cardCount);
   await expect(page.locator(".card-text")).toHaveCount(cardCount);
 });
 
-test("has working filters", async ({ page }) => {
-  await expect(page.locator(".list-group-item")).toContainText([
-    "CATEGORIES",
-    "Phones",
-    "Laptops",
-    "Monitors",
-  ]);
-
-  await page.locator(".card").first().waitFor();
-  const totalCount = await page.locator(".card-block").count();
-
+test("phones filter shows only phones", async ({ page }) => {
   await page.locator(".list-group-item", { hasText: "Phones" }).click();
-  await expect(page.locator(".card")).not.toHaveCount(totalCount);
+  await expect(page.locator(".card")).toContainText(CATEGORIES.phones);
+  await expect(page.locator(".card")).not.toContainText([
+    ...CATEGORIES.monitors,
+    ...CATEGORIES.laptops,
+  ]);
+});
 
-  const filterCount = await page.locator(".card-block").count();
+test("laptops filter shows only laptops", async ({ page }) => {
+  await page.locator(".list-group-item", { hasText: "Laptops" }).click();
+  await expect(page.locator(".card")).toContainText(CATEGORIES.laptops);
+  await expect(page.locator(".card")).not.toContainText([
+    ...CATEGORIES.monitors,
+    ...CATEGORIES.phones,
+  ]);
+});
 
-  expect(filterCount).toBeLessThan(totalCount);
+test("monitors filter shows only monitors", async ({ page }) => {
+  await page.locator(".list-group-item", { hasText: "Monitors" }).click();
+  await expect(page.locator(".card")).toContainText(CATEGORIES.monitors);
+  await expect(page.locator(".card")).not.toContainText([
+    ...CATEGORIES.phones,
+    ...CATEGORIES.laptops,
+  ]);
 });
 
 test("product card leads to product page", async ({ page }) => {
@@ -65,7 +74,7 @@ test("product card leads to product page", async ({ page }) => {
 });
 
 test("has working pagination", async ({ page }) => {
-  await page.locator(".card").first().waitFor();
+  await expect.soft(page.locator(".card").first()).toBeVisible();
   const pageOneProduct = await page.locator(".card-title").last().innerText();
 
   await page.locator(".page-item #next2", { hasText: "Next" }).click();
@@ -73,8 +82,6 @@ test("has working pagination", async ({ page }) => {
     pageOneProduct,
   );
 
-  await page.locator(".card").first().waitFor();
   await page.locator(".page-item #prev2", { hasText: "Previous" }).click();
-
   await expect(page.locator(".card-title")).toContainText([pageOneProduct]);
 });
