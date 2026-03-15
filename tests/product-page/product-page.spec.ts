513b6fda951ada78afd3b77adc126b9cd6f09032
BorisPrototyp <boris@prototyp.digital>
2026-03-15 12:15:01 +0100

    added product page tests

---
+++ tests/product-page/product-page.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/prod.html?idp_=1");
+});
+
+test("product page has neccessary elements ", async ({ page }) => {
+  await expect(page.locator(".name")).toBeVisible();
+  await expect(
+    page.locator(".price-container", { hasText: "$" }),
+  ).toBeVisible();
+  await expect(
+    page.locator("#more-information", { hasText: "Product description" }),
+  ).toBeVisible();
+  await expect(
+    page.locator("html").getByRole("link", { name: "Add to cart" }),
+  ).toBeVisible();
+  await expect(page.locator("#imgp img")).toBeVisible();
+});
+
+test("successfully adds product to cart ", async ({ page }) => {
+  page.on("dialog", async (dialog) => {
+    expect(dialog.message()).toBe("Product added");
+    await dialog.accept();
+  });
+
+  await page.locator("html").getByRole("link", { name: "Add to cart" }).click();
+});
