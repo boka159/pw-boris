76152b7f0cf0702d2cd999452e620446417bced6
BorisPrototyp <boris@prototyp.digital>
2026-04-29 22:54:45 +0200

    added more UI tests on homepage

--- tests/homepage/ui.spec.ts
+++ tests/homepage/ui.spec.ts
@@ -21,4 +21,19 @@ test("product cards have rendered img", async ({ page }) => {
   }
 });
 
-test("carousel has rendered img", async ({ page }) => {});
+test("carousel has rendered img", async ({ page }) => {
+  await expect(page.locator(".carousel-item").first()).toBeVisible();
+
+  const carouselImages = await page.locator(".carousel-item img").all();
+
+  for (const img of carouselImages) {
+    await expect(img).toHaveAttribute("src", /.jpg/);
+  }
+});
+
+test("categories filter has linear gradient", async ({ page }) => {
+  await expect(page.locator("#cat")).toHaveCSS(
+    "background-image",
+    "linear-gradient(135deg, rgb(76, 0, 255), rgb(0, 207, 255))",
+  );
+});
