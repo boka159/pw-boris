6cb15f505c5ec2280978cdcffb076a8d7b5e27d5
BorisPrototyp <boris@prototyp.digital>
2026-05-07 22:16:31 +0200

    finished navbar pom and used it in login test file

--- pages/navbar.page.ts
+++ pages/navbar.page.ts
@@ -4,17 +4,43 @@ export class NavBar {
   readonly page: Page;
   readonly loginButton: Locator;
   readonly signUpButton: Locator;
+  readonly logoutButton: Locator;
+  readonly contactButton: Locator;
+  readonly aboutUsButton: Locator;
+  readonly cartButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.loginButton = page.locator("#login2");
     this.signUpButton = page.locator("#signin2");
+    this.logoutButton = page.locator("#logout2");
+    this.contactButton = page.locator("#navbarExample a", {
+      hasText: "Contact",
+    });
+    this.aboutUsButton = page.locator("#navbarExample a", {
+      hasText: "About us",
+    });
+    this.cartButton = page.locator("#navbarExample a", {
+      hasText: "Cart",
+    });
   }
 
-  async clickOnLogIn() {
+  async clickOnLogin() {
     await this.loginButton.click();
   }
   async clickOnSignUp() {
     await this.signUpButton.click();
+  }
+  async clickOnLogout() {
+    await this.logoutButton.click();
+  }
+  async clickOnContact() {
+    await this.contactButton.click();
+  }
+  async clickOnAboutUs() {
+    await this.aboutUsButton.click();
+  }
+  async clickOnCart() {
+    await this.cartButton.click();
   }
 }
--- tests/authentification/login.spec.ts
+++ tests/authentification/login.spec.ts
@@ -2,98 +2,99 @@ import { test, expect } from "@playwright/test";
 import { CREDENTIALS } from "../../testData";
 import { closeModalOnButton, closeModalOnX } from "../../helpers/closeModal";
 import { login } from "../../helpers/login";
+import { NavBar } from "../../pages/navbar.page";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/");
-});
+test.describe("NavBar", () => {
+  let navbar: NavBar;
 
-test("Click on Log in opens login modal", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-  await expect(
-    page.locator(".modal-header h5", { hasText: "Log in" }),
-  ).toBeVisible();
-});
+  test.beforeEach(async ({ page }) => {
+    navbar = new NavBar(page);
 
-test("successful login shows welcome user and log out links", async ({
-  page,
-}) => {
-  await login(page, CREDENTIALS.username, CREDENTIALS.password);
-
-  await expect
-    .soft(page.locator("#logout2", { hasText: "Log out" }))
-    .toBeVisible();
-  await expect(
-    page.locator("#nameofuser", { hasText: "Welcome " + CREDENTIALS.username }),
-  ).toBeVisible();
-});
-
-test("empty credential field returns error", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-
-  page.on("dialog", async (dialog) => {
-    expect(dialog.message()).toBe("Please fill out Username and Password.");
-    await dialog.accept();
+    await page.goto("/");
   });
 
-  await page.locator("html").getByRole("button", { name: "Log in" }).click();
-});
-
-test("incorrect username returns error", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-  await page.locator("#loginusername").fill("wrong username");
-  await page.locator("#loginpassword").fill(CREDENTIALS.password);
-
-  page.on("dialog", async (dialog) => {
-    expect(dialog.message()).toBe("User does not exist.");
-    await dialog.accept();
+  test("Click on Log in opens login modal", async ({ page }) => {
+    await navbar.clickOnLogin();
+    await expect(
+      page.locator(".modal-header h5", { hasText: "Log in" }),
+    ).toBeVisible();
   });
 
-  await page.locator("html").getByRole("button", { name: "Log in" }).click();
-});
+  test("successful login shows welcome user and log out links", async ({
+    page,
+  }) => {
+    await login(page, CREDENTIALS.username, CREDENTIALS.password);
 
-test("incorrect password returns error", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-  await page.locator("#loginusername").fill(CREDENTIALS.username);
-  await page.locator("#loginpassword").fill("wrong_password");
-
-  page.on("dialog", async (dialog) => {
-    expect(dialog.message()).toBe("Wrong password.");
-    await dialog.accept();
+    await expect.soft(navbar.logoutButton).toBeVisible();
+    await expect(
+      page.locator("#nameofuser", {
+        hasText: "Welcome " + CREDENTIALS.username,
+      }),
+    ).toBeVisible();
   });
 
-  await page.locator("html").getByRole("button", { name: "Log in" }).click();
-});
+  test("empty credential field returns error", async ({ page }) => {
+    await navbar.clickOnLogin();
 
-test("click on X closes the modal", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-  await closeModalOnX(page, "Log in");
+    page.on("dialog", async (dialog) => {
+      expect(dialog.message()).toBe("Please fill out Username and Password.");
+      await dialog.accept();
+    });
 
-  await expect(
-    page.locator(".modal-header h5", { hasText: "Log in" }),
-  ).not.toBeVisible();
-});
+    await page.locator("html").getByRole("button", { name: "Log in" }).click();
+  });
 
-test("click on Close closes the modal", async ({ page }) => {
-  await page.locator("#navbarExample a", { hasText: "Log in" }).click();
-  await closeModalOnButton(page, "Log in");
+  test("incorrect username returns error", async ({ page }) => {
+    await navbar.clickOnLogin();
+    await page.locator("#loginusername").fill("wrong username");
+    await page.locator("#loginpassword").fill(CREDENTIALS.password);
 
-  await expect(
-    page.locator(".modal-header h5", { hasText: "Log in" }),
-  ).not.toBeVisible();
-});
+    page.on("dialog", async (dialog) => {
+      expect(dialog.message()).toBe("User does not exist.");
+      await dialog.accept();
+    });
 
-test("click on Log out logs the user out", async ({ page }) => {
-  await login(page, CREDENTIALS.username, CREDENTIALS.password);
+    await page.locator("html").getByRole("button", { name: "Log in" }).click();
+  });
 
-  await expect
-    .soft(page.locator("#logout2", { hasText: "Log out" }))
-    .toBeVisible();
-  await page.locator("#navbarExample a", { hasText: "Log out" }).click();
+  test("incorrect password returns error", async ({ page }) => {
+    await navbar.clickOnLogin();
+    await page.locator("#loginusername").fill(CREDENTIALS.username);
+    await page.locator("#loginpassword").fill("wrong_password");
 
-  await expect
-    .soft(page.locator("#navbarExample a", { hasText: "Log in" }))
-    .toBeVisible();
-  await expect(
-    page.locator("#navbarExample a", { hasText: "Sign up" }),
-  ).toBeVisible();
+    page.on("dialog", async (dialog) => {
+      expect(dialog.message()).toBe("Wrong password.");
+      await dialog.accept();
+    });
+
+    await page.locator("html").getByRole("button", { name: "Log in" }).click();
+  });
+
+  test("click on X closes the modal", async ({ page }) => {
+    await navbar.clickOnLogin();
+    await closeModalOnX(page, "Log in");
+
+    await expect(
+      page.locator(".modal-header h5", { hasText: "Log in" }),
+    ).not.toBeVisible();
+  });
+
+  test("click on Close closes the modal", async ({ page }) => {
+    await navbar.clickOnLogin();
+    await closeModalOnButton(page, "Log in");
+
+    await expect(
+      page.locator(".modal-header h5", { hasText: "Log in" }),
+    ).not.toBeVisible();
+  });
+
+  test("click on Log out logs the user out", async ({ page }) => {
+    await login(page, CREDENTIALS.username, CREDENTIALS.password);
+
+    await expect.soft(navbar.logoutButton).toBeVisible();
+    await navbar.clickOnLogout();
+
+    await expect.soft(navbar.loginButton).toBeVisible();
+    await expect(navbar.signUpButton).toBeVisible();
+  });
 });
